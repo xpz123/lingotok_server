7c361a0e8e5e2a3690a2e01ebd19b90d7d2b5312
gaoshaojun <gaoshaojun123@163.com>
2024-11-08 15:36:43 +0800

    add translating & time stamp huoshan API

---
+++ video_process/call_huoshan_srt.py
@@ -0,0 +1,66 @@
+import time
+import json
+import requests
+
+base_url = 'https://openspeech.bytedance.com/api/v1/vc'
+appid = "4822083580"
+access_token = "ICPlIxh2QEPMh1otaFjg0AqemFkuyv3a"
+
+language = 'en-US'
+# file_url = 'https://vdn.vzuu.com/FHD/4f0cd4d6-9a9a-11ef-bc05-fa03f967de9b-v8_f2_t1_le389l8W.mp4?auth_key=1730985590-0-0-778c2d55c0f733f85e67322e79fc8d08&bu=da4bec50&c=avc.8.0&disable_local_cache=1&expiration=1730985590&f=mp4&pu=1513c7c2&v=ali'
+
+
+# def log_time(func):
+#     def wrapper(*args, **kw):
+#         begin_time = time.time()
+#         func(*args, **kw)
+#         print('total cost time = {time}'.format(time=time.time() - begin_time))
+#     return wrapper
+
+
+def call_huoshan_srt(file_url):
+    response = requests.post(
+                 '{base_url}/submit'.format(base_url=base_url),
+                 params=dict(
+                     appid=appid,
+                     language=language,
+                     use_itn='True',
+                     use_capitalize='True',
+                     max_lines=1,
+                     words_per_line=55,
+                 ),
+                 json={
+                    'url': file_url,
+                 },
+                 headers={
+                    'content-type': 'application/json',
+                    'Authorization': 'Bearer; {}'.format(access_token)
+                 }
+             )
+    print('submit response = {}'.format(response.text))
+    assert(response.status_code == 200)
+    assert(response.json()['message'] == 'Success')
+
+    job_id = response.json()['id']
+    response = requests.get(
+            '{base_url}/query'.format(base_url=base_url),
+            params=dict(
+                appid=appid,
+                id=job_id,
+            ),
+            headers={
+               'Authorization': 'Bearer; {}'.format(access_token)
+            }
+    )
+    assert(response.status_code == 200)
+    res = response.json()
+    return res
+    # print('query response = {}'.format(response.json()))
+    # fw = open("485_srt_test.txt", "w")
+    # fw.write(json.dumps(response.json()))
+    # fw.close()
+    # assert(response.status_code == 200)
+
+if __name__ == '__main__':
+    pass
+    # main()
---
+++ video_process/translator.py
@@ -0,0 +1,31 @@
+import json
+
+from volcengine.ApiInfo import ApiInfo
+from volcengine.Credentials import Credentials
+from volcengine.ServiceInfo import ServiceInfo
+from volcengine.base.Service import Service
+
+def translate_text2ar(text_list, target_lang, source_lang="en"):
+    k_access_key = 'AKLTOTgzODg1Y2FiNDI5NGE3Mzk3MWEzYzJlODE3MDk2MzQ' # https://console.volcengine.com/iam/keymanage/
+    k_secret_key = 'TTJJM016azRaR0V3WXpRMk5EUXhPR0kyT0RBNVlUY3hZVGd5WlRrMlpHTQ=='
+    k_service_info = \
+        ServiceInfo('translate.volcengineapi.com',
+                    {'Content-Type': 'application/json'},
+                    Credentials(k_access_key, k_secret_key, 'translate', 'cn-north-1'),
+                    5,
+                    5)
+    k_query = {
+        'Action': 'TranslateText',
+        'Version': '2020-06-01'
+    }
+    k_api_info = {
+        'translate': ApiInfo('POST', '/', k_query, {}, {})
+    }
+    service = Service(k_service_info, k_api_info)
+    body = {
+        'TargetLanguage': target_lang,
+        'SourceLanguage': source_lang,
+        'TextList': text_list,
+    }
+    res = service.json('translate', {}, json.dumps(body))
+    return (json.loads(res))
