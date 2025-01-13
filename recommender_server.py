b416e2b3164cd830e6cc3a15f92727b4a5de94c8
shaojun <gaoshaojun123@163.com>
2025-01-14 00:47:32 +0800

    update recommender server

--- recommender_server.py
+++ recommender_server.py
@@ -1,8 +1,11 @@
 from recommender import Recommender
+from recommender_v1 import RecommenderV1
 from flask import Flask, request
 
 recommender = Recommender()
+recommender_v1 = RecommenderV1()
 app = Flask(__name__)
+
 @app.route('/recommend_video', methods=["POST"])
 def recommend_video():
     
@@ -11,6 +14,18 @@ def recommend_video():
     size = input_data["size"]
     try:
         video_info_list = recommender.get_video_with_username(uid, recommended_video_count=size)
+        video_id_list = [video_info['video_id'] for video_info in video_info_list]
+        return {"video_id_list": video_id_list, "code": 200}
+    except Exception as e:
+        print (e)
+        return {"code": -1, "video_id_list": []}
+
+@app.route('/recommend_video_v1', methods=["POST"])
+def recommend_video_v1():
+    
+    input_data = request.get_json()
+    try:
+        video_info_list = recommender_v1.reccommend(input_data)
         video_id_list = [video_info['video_id'] for video_info in video_info_list]
         return {"video_id_list": video_id_list, "code": 200}
     except Exception as e:
