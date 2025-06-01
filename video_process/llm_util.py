dd52966e94d5d38c0ce367bdbee0094a427ee384
shaojun <gaoshaojun123@163.com>
2025-06-01 14:05:30 +0800

    upload llm_util.py

--- video_process/llm_util.py
+++ video_process/llm_util.py
@@ -1,6 +1,18 @@
 import requests
 import json
 from openai import OpenAI
+from volcenginesdkarkruntime import Ark
+from concurrent.futures import ThreadPoolExecutor
+from typing import List, Any
+import base64
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+doubao_client = Ark(
+    # 此为默认路径，您可根据业务所在地域进行配置
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+    # 从环境变量中获取您的 API Key。此为默认方式，您可根据需要进行修改
+    api_key="09351b42-cf23-4549-8955-d338e2dfe9b6",
+)
 
 def call_doubao_pro_32k(prompt):
     client = OpenAI(
@@ -20,51 +32,80 @@ def call_doubao_pro_32k(prompt):
     return (completion.choices[0].message.content)
 
 def call_doubao_pro_128k(prompt):
-    url = "http://ai-service-test.tal.com/openai-compatible/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": "1000080817:9d63da093db44efb2668e263b0d1f06b"
-    }
-    data = {
-        "model": "doubao-pro-128k",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ]
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    return response.json()
+    completion = doubao_client.chat.completions.create(
+   # 指定您创建的方舟推理接入点 ID，此处已帮您修改为您的推理接入点 ID
+    model="doubao-1-5-pro-32k-250115",
+    messages=[
+        {"role": "system", "content": "你是人工智能翻译专家"},
+        {"role": "user", "content": prompt},
+    ],)
+    print(completion.choices[0].message.content)
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(3))
+def call_doubao_pro_15_32k(prompt):
+    try:
+        completion = doubao_client.chat.completions.create(
+    # 指定您创建的方舟推理接入点 ID，此处已帮您修改为您的推理接入点 ID
+        model="doubao-1-5-pro-32k-250115",
+        messages=[
+            {"role": "system", "content": "你是人工智能助手."},
+            {"role": "user", "content": prompt},
+        ],)
+    # print(completion.choices[0].message.content)
+        return completion.choices[0].message.content
+    except Exception as e:
+        print (e)
+        return ""
+
     # print(response.json())
 
-def call_gpt4o(prompt):
-    # url = "http://ai-service-test.tal.com/openai-compatible/v1/chat/completions"
-    url= "http://ai-service.tal.com/openai-compatible/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": "1000080777:97e29110ef8758e097f4c41cd67f19d6"
-    }
-    data = {
-        "model": "gpt-4o",
-        "messages": [
+def call_doubao_vl_1_5(prompt, image_path):
+    # 读取并编码本地图片
+    with open(image_path, "rb") as image_file:
+        image_base64 = base64.b64encode(image_file.read()).decode('utf-8')
+
+    response = doubao_client.chat.completions.create(
+        model="doubao-1.5-vision-pro-250328",
+        messages=[
             {
                 "role": "user",
-                "content": prompt
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image_base64}"
+                        },
+                    },
+                    {"type": "text", "text": prompt},
+                ],
             }
-        ]
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    return response.json()
+        ],
+    )
+    
+    return response.choices[0].message.content
+
+def call_api_parallel(func, prompts, max_workers=5):
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        results = list(executor.map(func, prompts))
+    return results
+
+def call_doubao_pro_15_32k_parallel(prompts: List[str], max_workers: int = 5) -> List[Any]:
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        results = list(executor.map(call_doubao_pro_15_32k, prompts))
+    return results
 
 if __name__ == "__main__":
-    # pass
-    import json
-    data ={"question": "森林中弥漫的清新气息是大自然赋予的独特__？", "options": ["A. 纹理", "B. 质地", "C. 韵味", "D. 馈赠"], "answer": "D", "explanation": "森林中弥漫的清新气息是大自然给予的，‘纹理’和‘质地’通常用来形容物体的表面特性，‘韵味’侧重于表达某种含蓄的意味，都不合适，而‘馈赠’能准确表达大自然给予的意思，所以应选 D 选项‘馈赠’。"}
-    json_str = json.dumps(data,  ensure_ascii=False)
-    # print (json_str)
-    resp = (call_doubao_pro_128k("以下是一个中文句子，“荷花 全身 上下 所 积蓄 的 夏日 能量”，如果我遮挡其中“能量”这个词之后，将该句子变成一个选择题目，其中“能量“是正确选项，而其他词则是和“能量” 不想进并且也不符合语法语义的词。请注意！给我的结果需要按照如下的json格式: {}".format(json_str)))
-    # resp = (call_gpt4o("以下是一个中文句子，“荷花 全身 上下 所 积蓄 的 夏日 能量”，如果我遮挡其中“能量”这个词之后，将该句子变成一个选择题目，其中“能量“是正确选项，而其他词则是和“能量” 不想进并且也不符合语法语义的词。请注意！给我的结果需要按照如下的json格式: {}".format(json_str)))
-    print (resp)
+    result = call_doubao_vl_1_5("你是一个中文老师，你想要向同学们介绍 人 车 公交车 出租车 晴天 太阳 等词语。分析一下这个图片，你可以通过这个视频介绍什么简单的汉字或词语？ ##请注意，只需要返回图片中最有代表性的一个词，并且只返回这个词或字，不要输出其他内容。", "/Users/tal/work/lingtok_server/video_process/自制视频/视频加文字/1/frame_0510.jpg")
+    print (result)
+    # print (call_gemini_2_5_pro("请把下面的中文翻译成阿拉伯语：森林中弥漫的清新气息是大自然赋予的独特"))
+    # print (call_gpt4o("请把下面的中文翻译成阿拉伯语：森林中弥漫的清新气息是大自然赋予的独特"))
+    # print (call_doubao_pro_15_32k("请把下面的中文翻译成英文：森林中弥漫的清新气息是大自然赋予的独特"))
+    # prompts = ["请把下面的中文翻译成英文：森林中弥漫的清新气息是大自然赋予的独特", "请把下面的中文翻译成英文：森林中弥漫的清新气息是大自然赋予的独特"] * 10
+    # results = call_gpto1_preview("请把下面的中文翻译成阿拉伯语：森林中弥漫的清新气息是大自然赋予的独特")
+    # prompts = ["请把下面的中文翻译成阿拉伯语：森林中弥漫的清新气息是大自然赋予的独特"] * 3
+    # results = call_api_parallel(call_claude_3_7, prompts)
+    # print (results)
+    # results = call_doubao_pro_15_32k_parallel(prompts)
+    # import pdb;pdb.set_trace()
 
 
